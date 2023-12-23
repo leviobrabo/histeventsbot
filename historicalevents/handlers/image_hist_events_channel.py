7f1f8a0b306a9ec7feb4e82cb5b34c48ecef9107
leviobrabo <carlosjunior20313@gmail.com>
2023-12-23 18:52:21 -0300

    [FIX] - Correcting image upload and description of different historical facts

--- historicalevents/handlers/image_hist_events_channel.py
+++ historicalevents/handlers/image_hist_events_channel.py
@@ -29,40 +29,32 @@ def send_historical_events_channel_image(CHANNEL):
             if event.get('pages') and event['pages'][0].get('thumbnail')
         ]
 
-        if events:
-            random_event = random.choice(events)
+        if events_with_photo:
+            random_event = random.choice(events_with_photo)
             event_text = random_event.get('text', '')
             event_year = random_event.get('year', '')
 
-        if not events_with_photo:
+            caption = f'<b>🖼 | Illustrated History </b>\n\nOn <b>{day} of {get_month_name(month)} {event_year}</b>\n\n<code>{event_text}</code>\n\n💬 Did you know? Follow @today_in_historys.'
 
+            options = {'parse_mode': 'HTML'}
+
+            photo_url = random_event['pages'][0]['thumbnail']['source']
+            bot.send_photo(CHANNEL, photo_url, caption=caption, **options)
+
+            logger.success(
+                f'Historical event in photo sent successfully to channel ID {CHANNEL}.'
+            )
+        else:
             logger.info('There are no events with photos to send today.')
 
-            return
-
-        random_event = random.choice(events_with_photo)
-        caption = f'<b>🖼 | Illustrated History </b>\n\nOn <b>{day} of {get_month_name(month)} {event_year}</b>\n\n<code>{event_text}</code>\n\n💬 Did you know? Follow @today_in_historys.'
-
-        options = {'parse_mode': 'HTML'}
-
-        photo_url = random_event['pages'][0]['thumbnail']['source']
-        bot.send_photo(CHANNEL, photo_url, caption=caption, **options)
-
-        logger.success(
-            f'Historical event in photo sent successfully to channel ID {CHANNEL}.'
-        )
-
     except Exception as e:
-
         logger.error(f'Failed to send historical event: {e}')
 
 
 def hist_channel_imgs():
     try:
         send_historical_events_channel_image(CHANNEL)
-
         logger.success(f'Message sent to channel {CHANNEL}')
 
     except Exception as e:
-
         logger.error('Error sending image job:', str(e))
