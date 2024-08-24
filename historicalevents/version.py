212677c848197beab6a1caa8fa8888939f4099a7
leviobrabo <carlosjunior20313@gmail.com>
2024-08-24 15:25:55 -0300

    update version

--- historicalevents/version.py
+++ historicalevents/version.py
@@ -3,7 +3,7 @@ from sys import version_info
 import telebot
 
 python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
-histevents_version = '2.6.9'
+histevents_version = '3.2.9'
 telebot_version = (
     telebot.__version__
     if hasattr(telebot, '__version__')
